afd4f109b96fc6d4fe43f64ebf88b0309b942188
Ramon Barros <ramon@ezoom.com.br>
2013-09-19 14:03:29 -0300

    Start of modifying the structure of directories.

---
+++ src/Core/AI.h
@@ -0,0 +1,13 @@
+#ifndef CORE_AI_INCLUDED
+#define CORE_AI_INCLUDED
+
+#include <iostream>
+#include <locale>
+#include <exception>
+using namespace std;
+
+namespace AI {
+    class Expection : public std::exception {};
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "Core/AI.h"
+
+int main() {
+    setlocale(LC_ALL, "C");
+    cout << "AI initialized" << endl;
+    //printf("AI initialized");
+    return 0;
+}
